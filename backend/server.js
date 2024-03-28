8c0f5653d5f8ad3098c2c6e8ed84245e9a0800e2
Diogo Meira <diogomeira81@gmail.com>
2024-03-28 11:07:25 +0000

    Add CORS handling middleware

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,26 @@ app.use('/api/messages', messagesRoutes)
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
+})
 
+// Add CORS handling middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        return res.status(200).json({});
+    }
+    next();
+});
 
-  mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-      app.listen(process.env.PORT, () => {
-          console.log("Connected to db & listening on port", process.env.PORT);
-      });
-  })
-  .catch((error) => {
-      console.error("Error connecting to the database:", error);
-      process.exit(1); // Exit the process if unable to connect to the database
-  });
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("Connected to db & listening on port", process.env.PORT);
+    });
+})
+.catch((error) => {
+    console.error("Error connecting to the database:", error);
+    process.exit(1); // Exit the process if unable to connect to the database
+});
