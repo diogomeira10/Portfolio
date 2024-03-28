16537c035db5bcb68b2bf4f0e8c7b2e358b452f4
Diogo Meira <diogomeira81@gmail.com>
2024-03-28 10:55:43 +0000

    npm install cors package

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 const messagesRoutes = require('./routes/messagesRoutes')
 
 const express = require('express')
-
+const cors = require('cors')
+const app  = express()
 const mongoose = require('mongoose')
 
-const app  = express()
+app.use(cors())
 
 app.use(express.json())
 
--- frontend/src/components/TimeTrack.js
+++ frontend/src/components/TimeTrack.js
@@ -1,6 +1,7 @@
 import TimeTrack1 from '../assets/Projects/TimeTrack-1.svg'
 import TimeTrack2 from '../assets/Projects/TimeTrack-2.svg'
 import Logo from '../assets/Projects/TimeTrack-Logo.svg'
+
 // import { ArrowButton } from './ArrowButton'
 
 export const TimeTrack = () => {
